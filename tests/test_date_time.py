cb8b710f84941f257a24e7d031fd53d3986eef0d
Aman Kumar Verma <darkstardreamisp@gmail.com>
2023-03-20 22:24:49 +0530

    removed tuple with single value in test_convert_time

--- tests/test_date_time.py
+++ tests/test_date_time.py
@@ -4,32 +4,31 @@ from ixia.date_time import (
     _convert_time,
     _microseconds,
     rand_date,
-    rand_time
+    rand_time,
 )
-
 
 def test_convert_date():
     # with input as str
-    assert dt.date(2022, 1, 1) == _convert_date('2022-01-01')
-    
+    assert dt.date(2022, 1, 1) == _convert_date("2022-01-01")
+
     # with input as int
     assert dt.date(2022, 1, 1) == _convert_date(2022)
-    
+
     # with input as tuple
     assert dt.date(2022, 1, 1) == _convert_date((2022, 1, 1))
-    
+
     # with input as datetime
     assert dt.date(2022, 1, 1) == _convert_date(dt.datetime(2022, 1, 1, 12, 00, 00))
 
+
 def test_convert_time():
     # with input as str
-    assert dt.time(12) == _convert_time('12:00:00')
+    assert dt.time(12) == _convert_time("12:00:00")
 
     # with input as int
     assert dt.time(12) == _convert_time(12)
-    
+
     # with input as tuple
-    assert dt.time(12) == _convert_time((12))
     assert dt.time(12) == _convert_time((12, 0))
     assert dt.time(12) == _convert_time((12, 0, 0))
     assert dt.time(12) == _convert_time((12, 0, 0, 0))
@@ -37,14 +36,18 @@ def test_convert_time():
     # with input as datetime
     assert dt.time(12) == _convert_time(dt.datetime(2022, 1, 1, 12, 00, 00))
 
+
 def test_microseconds():
     time = dt.time(hour=6, minute=45, second=0)
-    microseconds = ((time.hour * 3600) + (time.minute * 60) + time.second) * 1000000 + time.microsecond
+    microseconds = (
+        (time.hour * 3600) + (time.minute * 60) + time.second
+    ) * 1000000 + time.microsecond
     assert microseconds == _microseconds(time)
 
+
 def test_rand_date():
-    start = dt.date(2022,1,1)
-    end = dt.date(2022,12,31)
+    start = dt.date(2022, 1, 1)
+    end = dt.date(2022, 12, 31)
 
     # with both start and end
     assert start <= rand_date(start, end) <= end
@@ -52,6 +55,7 @@ def test_rand_date():
 
     # without end
     assert start <= rand_date(start) <= end
+
 
 def test_rand_time():
     start = dt.time(6, 45, 00)
@@ -68,4 +72,3 @@ def test_rand_time():
 
     # with none as input
     assert dt.time.min <= rand_time() <= dt.time.max
-
