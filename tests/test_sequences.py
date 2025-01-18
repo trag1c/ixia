50ee1e385237fe918aefd58e2d5cbe19b7fb941b
trag1c <trag1cdev@yahoo.com>
2025-01-18 18:02:47 +0100

    test: add missing sequences module test cases

--- tests/test_sequences.py
+++ tests/test_sequences.py
@@ -1,18 +1,21 @@
+import re
 from pathlib import Path
+from typing import Any
 from unittest.mock import patch
 
 import pytest
 
-from ixia import passphrase, rand_line, shuffled
+from ixia import choice, choices, passphrase, rand_line, sample, shuffled
 
 TEST_LIST = [6, 3, 9, 1, 2, 4, 8, 0, 5, 7]
+TEST_TUPLE = tuple(TEST_LIST)
 
 
 def test_shuffled() -> None:
     for _ in range(300):
-        s = shuffled(TEST_LIST)
-        assert TEST_LIST is not s
-        assert sorted(s) == sorted(TEST_LIST)
+        a = shuffled(TEST_LIST)
+        b = shuffled(TEST_TUPLE)
+        assert sorted(a) == sorted(b) == sorted(TEST_LIST)
 
 
 def test_rand_line(tmp_path: Path) -> None:
@@ -37,3 +40,100 @@ def test_passphrase_nonexistent() -> None:
         path_inst.__eq__.return_value = True
         with pytest.raises(NotImplementedError):
             passphrase(1)
+
+
+def test_choice() -> None:
+    for _ in range(100):
+        assert choice(range(1000)) in range(1000)
+
+
+def test_choice_empty_seq() -> None:
+    with pytest.raises(
+        IndexError, match=re.escape("Cannot choose from an empty sequence")
+    ):
+        choice([])
+
+
+def test_choice_with_weights() -> None:
+    assert choice([1, 2], weights=[0, 1]) == 2
+
+
+@pytest.mark.parametrize(
+    ("args", "kwargs", "exc_type", "exc_msg"),
+    [
+        (
+            ([1], 1),
+            {},
+            TypeError,
+            "The number of choices must be a keyword argument: k=1",
+        ),
+        (([1], 1.0), {}, TypeError, "'float' object is not iterable"),
+        (
+            ([1], [1]),
+            {"cumulative_weights": [1]},
+            TypeError,
+            "Cannot specify both weights and cumulative weights",
+        ),
+        (
+            ([1],),
+            {"cumulative_weights": [1, 2]},
+            ValueError,
+            "The number of weights does not match the sequence",
+        ),
+        (([0], [0]), {}, ValueError, "Total of weights must be greater than zero"),
+        (([0], [1e309]), {}, ValueError, "Total of weights must be finite"),
+    ],
+)
+def test_choices_erroneous_cases(
+    args: tuple[Any, ...],
+    kwargs: dict[str, Any],
+    exc_type: type[BaseException],
+    exc_msg: str,
+) -> None:
+    with pytest.raises(exc_type, match=re.escape(exc_msg)):
+        choices(*args, **kwargs)
+
+
+def test_sample() -> None:
+    base = range(21, 37)
+    for k in range(17):
+        s = sample(base, k)
+        assert len(s) == k
+        assert set(s) <= set(base)
+
+
+def test_sample_counts() -> None:
+    assert sample([1, 2], 1, counts=[1, 0]) == [1]
+    assert sample([1, 2], 1, counts=[1, 10**10]) == [2]
+    assert sample([1, 2], 50, counts=[0, 50]) == [2] * 50
+    assert sample([1, 2], 50, counts=[0, 500]) == [2] * 50
+
+
+@pytest.mark.parametrize(
+    ("args", "kwargs", "exc_type", "exc_msg"),
+    [
+        (([1], 2), {}, ValueError, "Sample larger than sequence or is negative"),
+        (([1], -1), {}, ValueError, "Sample larger than sequence or is negative"),
+        (
+            ([1], 1),
+            {"counts": [1, 2]},
+            ValueError,
+            "The number of counts does not match the sequence",
+        ),
+        (([1, 2], 1), {"counts": "12"}, TypeError, "Counts must be integers"),
+        (
+            ([1], 1),
+            {"counts": [0]},
+            ValueError,
+            "Total of counts must be greater than zero",
+        ),
+    ],
+)
+def test_sample_erroneous_cases(
+    args: tuple[Any, ...],
+    kwargs: dict[str, Any],
+    exc_type: type[BaseException],
+    exc_msg: str,
+) -> None:
+    with pytest.raises(exc_type, match=re.escape(exc_msg)):
+        sample(*args, **kwargs)
